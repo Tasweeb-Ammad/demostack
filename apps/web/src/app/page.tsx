2db69960c34484ea969ee262978b2e816b7e3337
Tasweeb Ammad <ammadtasweeb@gmail.com>
2025-09-17 15:48:55 +0500

    added logout

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import { useAuthInfo } from "@propelauth/react";
+import { Button } from "@/components/ui/button";
+import { useAuthInfo, useLogoutFunction } from "@propelauth/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { loading, isLoggedIn, user } = useAuthInfo();
+  const logout = useLogoutFunction();
+  const router = useRouter();
 
   console.log(user);
 
@@ -17,6 +21,16 @@ export default function Home() {
     <div>
       <p>Welcome, {user.email}</p>
       <p>User ID: {user.userId}</p>
+      <Button
+        type="submit"
+        className="w-fit cursor-pointer"
+        onClick={async () => {
+          await logout(false);
+          router.replace("/sign-in");
+        }}
+      >
+        Logout
+      </Button>
     </div>
   );
 }
